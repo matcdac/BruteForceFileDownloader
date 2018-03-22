fcbcd8a80d51a162c918942eca5693a3c15b50e3
matcdac <matcdac@gmail.com>
2018-03-22 10:52:06 +0530

    Added code to generate characters, for one path variable in URL, containing a-z A-Z 0-9 _

---
+++ HitApi.java
@@ -0,0 +1,109 @@
+package d;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class HitApi {
+
+	private String basePath = null;
+	private Set<String> extensions = null;
+	private int minChars;
+	private int maxChars;
+	
+	private static List<Character> urlCharacters = new ArrayList<Character>();
+	private static Set<Character> urlSpecialCharacters = new HashSet<Character>();
+	
+	private Map<Integer, List<Integer>> lengthVsNextGeneratePositionMapping = null;
+	private Map<Integer, Boolean> digitVsProgress = null;
+	
+	static {
+		addCharacters();
+		addSpecialCharacters();
+	}
+
+	private static void addCharacters() {
+		int ascii;
+		for(ascii = 97; ascii <= 122; ascii++)
+			urlCharacters.add((char) ascii);
+		for(ascii = 65; ascii <= 90; ascii++)
+			urlCharacters.add((char) ascii);
+		for(ascii = 48; ascii <= 57; ascii++)
+			urlCharacters.add((char) ascii);
+		// underscore
+		urlCharacters.add((char) 95);
+	}
+	
+	private static void addSpecialCharacters() {
+		int[] special = { 36, 38, 43, 44, 47, 58, 59, 61, 63, 64, 32, 34, 60, 62, 35, 37, 123, 125, 124, 92, 94, 126, 91, 93, 96, 46};
+		for(int ch: special)
+			urlSpecialCharacters.add((char) ch);
+	}
+	
+	public HitApi(String basePath, Set<String> extensions, int minChars, int maxChars) {
+		this.basePath = basePath;
+		this.extensions = extensions;
+		this.minChars = minChars;
+		this.maxChars = maxChars;
+		lengthVsNextGeneratePositionMapping = new HashMap<Integer, List<Integer>>();
+		digitVsProgress = new HashMap<Integer, Boolean>();
+		for(int n = minChars; n <= maxChars; n++) {
+			List<Integer> listOfPositionsOfSizeN = new ArrayList<Integer>(n);
+			lengthVsNextGeneratePositionMapping.put(n, listOfPositionsOfSizeN);
+			digitVsProgress.put(n, false);
+		}
+		digitVsProgress.put(minChars, true);
+	}
+	
+	private Integer getWordLengthToBeGenerated() {
+		for(Integer digit : digitVsProgress.keySet()) {
+			if(digitVsProgress.get(digit)) {
+				return digit;
+			}
+		}
+		return null;
+	}
+	
+	private String getNextWord() {
+		Integer length = getWordLengthToBeGenerated();
+		List<Integer> nextGeneratePosition = null;
+		if(null != length)
+			nextGeneratePosition = lengthVsNextGeneratePositionMapping.get(length);
+		if(null != nextGeneratePosition) {
+			StringBuffer word = new StringBuffer();
+			for(int indexPointer : nextGeneratePosition) {
+				word.append(urlCharacters.get(indexPointer));
+			}
+			incrementProgress(length, nextGeneratePosition);
+			return word.toString();
+		} else {
+			return null;
+		}
+	}
+
+	private void incrementProgress(Integer currentLength, List<Integer> currentPosition) {
+		for(int index = currentLength-1; index >= 0; index--) {
+			int value = currentPosition.get(index);
+			if(value == urlCharacters.size()-1) {
+				currentPosition.add(index, 0);
+				if(index == 0) {
+					if(currentLength == maxChars) {
+						digitVsProgress.put(currentLength, false);
+					} else {
+						digitVsProgress.put(currentLength, false);
+						digitVsProgress.put(currentLength+1, true);
+					}
+				} else {
+					continue;
+				}
+			} else {
+				currentPosition.add(index, value+1);
+				break;
+			}
+		}
+	}
+
+}
---
+++ Start.java
@@ -0,0 +1,200 @@
+package d;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Start {
+	
+	private static String basePath = "http://www.mca.gov.in/Ministry/pdf/";
+	private static Set<String> extensions = new HashSet<String>();
+
+	private static void addText() {
+		extensions.add(".txt");
+		extensions.add(".rtf");
+	}
+	
+	private static void addCommaSeparated() {
+		extensions.add(".csv");
+	}
+	
+	private static void addXML() {
+		extensions.add(".xml");
+	}
+	
+	private static void addDB() {
+		extensions.add(".db");
+		extensions.add(".dbf");
+		extensions.add(".sql");
+	}
+	
+	private static void addBackup() {
+		extensions.add(".bak");
+	}
+	
+	private static void addDump() {
+		extensions.add(".dmp");
+	}
+	
+	private static void addConfiguration() {
+		extensions.add(".cfg");
+	}
+	
+	private static void addMicrosoftWordLegacy() {
+		extensions.add(".doc");
+		extensions.add(".dot");
+		extensions.add(".wbk");
+	}
+	
+	private static void addMicrosoftWord() {
+		extensions.add(".docx");
+		extensions.add(".docm");
+		extensions.add(".dotx");
+		extensions.add(".dotm");
+		extensions.add(".docb");
+	}
+	
+	private static void addMicrosoftExcelLegacy() {
+		extensions.add(".xls");
+		extensions.add(".xlt");
+		extensions.add(".xlm");
+	}
+	
+	private static void addMicrosoftExcel() {
+		extensions.add(".xlsx");
+		extensions.add(".xlsm");
+		extensions.add(".xltx");
+		extensions.add(".xltm");
+	}
+	
+	private static void addMicrosoftExcelOthers() {
+		extensions.add(".xlsb");
+		extensions.add(".xla");
+		extensions.add(".xlam");
+		extensions.add(".xll");
+		extensions.add(".xlw");
+	}
+	
+	private static void addMicrosoftPowerPointLegacy() {
+		extensions.add(".ppt");
+		extensions.add(".pot");
+		extensions.add(".pps");
+	}
+	
+	private static void addMicrosoftPowerPointOOXML() {
+		extensions.add(".pptx");
+		extensions.add(".pptm");
+		extensions.add(".potx");
+		extensions.add(".potm");
+		extensions.add(".ppam");
+		extensions.add(".ppsx");
+		extensions.add(".ppsm");
+		extensions.add(".sldx");
+		extensions.add(".sldm");
+	}
+	
+	private static void addMicrosoftWorks() {
+		extensions.add(".xlr");
+		extensions.add(".wks");
+		extensions.add(".wps");
+	}
+	
+	private static void addMicrosoftAccessLegacy() {
+		extensions.add(".ade");
+		extensions.add(".adp");
+		extensions.add(".mdt");
+		extensions.add(".ldb");
+	}
+	
+	private static void addMicrosoftAccessDatabaseLegacy() {
+		extensions.add(".mdb");
+		extensions.add(".cdb");
+		extensions.add(".mda");
+		extensions.add(".mdn");
+		extensions.add(".mdf");
+		extensions.add(".mde");
+	}
+	
+	private static void addMicrosoftAccess() {
+		extensions.add(".adn");
+		extensions.add(".accda");
+		extensions.add(".mdw");
+		extensions.add(".mam");
+		extensions.add(".maq");
+		extensions.add(".mar");
+		extensions.add(".mat");
+		extensions.add(".maf");
+		extensions.add(".laccdb");
+	}
+	
+	private static void addMicrosoftAccessDatabase() {
+		extensions.add(".accdb");
+		extensions.add(".accde");
+		extensions.add(".accdt");
+		extensions.add(".accdr");
+	}
+	
+	private static void addMicrosoftPublisher() {
+		extensions.add(".pub");
+	}
+	
+	private static void addOpenOfficeCalcSpreadsheet() {
+		extensions.add(".ods");
+	}
+	
+	private static void addOpenOfficeWriterDocument() {
+		extensions.add(".odt");
+	}
+	
+	private static void addPDF() {
+		extensions.add(".pdf");
+	}
+	
+	private static void addLatexDocument() {
+		extensions.add(".tex");
+	}
+	
+	private static void addWordPerfect() {
+		extensions.add(".wpd");
+	}
+	
+	private static void addXPS() {
+		extensions.add(".xps");
+	}
+	
+	private static void addExtensions() {
+		addText();
+		addCommaSeparated();
+		addXML();
+		addDB();
+		addBackup();
+		addDump();
+		addConfiguration();
+		addMicrosoftWordLegacy();
+		addMicrosoftWord();
+		addMicrosoftExcelLegacy();
+		addMicrosoftExcel();
+		addMicrosoftExcelOthers();
+		addMicrosoftPowerPointLegacy();
+		addMicrosoftPowerPointOOXML();
+		addMicrosoftWorks();
+		addMicrosoftAccessLegacy();
+		addMicrosoftAccessDatabaseLegacy();
+		addMicrosoftAccess();
+		addMicrosoftAccessDatabase();
+		addMicrosoftPublisher();
+		addOpenOfficeCalcSpreadsheet();
+		addOpenOfficeWriterDocument();
+		addPDF();
+		addLatexDocument();
+		addWordPerfect();
+		addXPS();
+	}
+
+	public static void main(String[] args) {
+		addExtensions();
+		HitApi hitApi = new HitApi(basePath, extensions, 1, 2);
+	}
+	
+	
+	
+}
